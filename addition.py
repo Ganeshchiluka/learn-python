0e251d9de270f018eed3afc727f66db7e9944fe3
Ganesh Chiluka <ganeshchiluka01@gmail.com>
2025-12-28 18:28:04 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,4 +1,4 @@
 first_number = int(input("Enter first number: "))
 second_number = int(input("Enter second number: "))
 sum_of_two_numbers = first_number + second_number 
-print("The Addition of the "+str(first_number)+" and "+str(second_number)+" is "+str(sum_of_two_numbers))
+print("The Addition of ",str(first_number)," and ",str(second_number)," is ",str(sum_of_two_numbers),".")
